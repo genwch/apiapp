9c97a4728259d158142ef4abb914467a517cbf45
genwch <genwch@hotmail.com>
2021-02-04 04:54:53 +0000

    fix sec path

--- apiapp/resources/__init__.py
+++ apiapp/resources/__init__.py
@@ -12,6 +12,7 @@ def init_routes(api):
               f"{apiroot}/data/<sec>/<model>/<id>", f"{apiroot}/data/<sec>/<model>/<col>/<id>"]
     apilst = []
     seclst = [1, 2, 3, 9]
+    secpath = ["public", "protected", "private", "auth"]
     for i in range(len(urllst)):
         for s in seclst:
             api.add_resource(dataapi, urllst[i],
@@ -24,4 +25,4 @@ def init_routes(api):
                      resource_class_kwargs={"model_path": "./model"}, endpoint="map_d")
     for s in seclst:
         api.add_resource(mapapi, f"{apiroot}/data/{s}", resource_class_args=(apilst),
-                         resource_class_kwargs={"model_path": "./model/public"}, endpoint=f"map_d_s{s}")
+                         resource_class_kwargs={"model_path": f"./model/{secpath[s]}"}, endpoint=f"map_d_s{s}")
